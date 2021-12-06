fbd8ccff3c051392d23f5bc9ee5e531f14dc05a8
Desirree Adegunle <87389186+dess890@users.noreply.github.com>
2021-12-06 10:05:30 -0500

    working but with bootstrap

---
+++ script.js
@@ -0,0 +1,23 @@
+// function addListItems(textContent) {
+//     const itemsNotDone = document.createElement("li")
+//     itemsNotDone.innerHTML = textContent
+//     const list = document.querySelector("#incomplete-items")
+//     list.appendChild(itemsNotDone)
+// }
+// addListItems("Learn React")
+// addListItems("Learn Node")
+// addListItems("Learn Express")
+
+// function addAnotherList(textContent) {
+//     const itemsDone = document.createElement("li")
+//     itemsDone.innerHtml = textContent
+//     const anotherList = document.querySelector("#complete-items")
+//     anotherList.appendChild(itemsDone)
+// }
+// addAnotherList("Learn React")
+// addAnotherList("Learn Node")
+// addAnotherList("Learn Express")
+
+
+//my javascript didnt work :( cheated with bootstrap!
+
